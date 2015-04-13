423336b3b34505c85924073f5381e96d48c1c199
Andy3942 <aa394215561@gmail.com>
2015-04-13 16:37:09 +0800

    create projects

---
+++ openglesTest/openglesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  openglesTest
+//
+//  Created by bzx on 4/13/15.
+//  Copyright (c) 2015 bzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ openglesTest/openglesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openglesTest
+//
+//  Created by bzx on 4/13/15.
+//  Copyright (c) 2015 bzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
